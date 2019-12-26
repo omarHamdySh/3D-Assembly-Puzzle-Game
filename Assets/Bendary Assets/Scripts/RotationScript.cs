285ec3aacd7a8e0630b1aa89d4e65aedea81a06b
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-12-26 09:52:39 +0200

    bendary

--- Assets/Bendary Assets/Scripts/RotationScript.cs
+++ Assets/Bendary Assets/Scripts/RotationScript.cs
@@ -1,36 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class RotationScript : MonoBehaviour
+public class RotationScript : Editor
 {
-    private Vector3 posDelta = Vector3.zero;
+    //private Vector3 posDelta = Vector3.zero;
 
-    private void OnMouseDown()
-    {
-        posDelta = Input.mousePosition;
-    }
+    //private void OnMouseDown()
+    //{
+    //    posDelta = Input.mousePosition;
+    //}
 
-    private void OnMouseDrag()
-    {
-        posDelta = Input.mousePosition - posDelta;
-        RotateShape(posDelta);
-    }
+    //private void OnMouseDrag()
+    //{
+    //    posDelta = Input.mousePosition - posDelta;
+    //    RotateShape(posDelta);
+    //}
 
-    public void RotateShape(Vector3 posDelta)
-    {
-        if (Vector3.Dot(transform.up, Vector3.up) >= 0)
-        {
-            // Rotate Around Y Axis
-            transform.Rotate(transform.up, -Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
-        }
-        else
-        {
-            // Rotate Around Y Axis but inverted
-            transform.Rotate(transform.up, Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
-        }
+    //public void RotateShape(Vector3 posDelta)
+    //{
+    //    if (Vector3.Dot(transform.up, Vector3.up) >= 0)
+    //    {
+    //        // Rotate Around Y Axis
+    //        transform.Rotate(transform.up, -Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+    //    }
+    //    else
+    //    {
+    //        // Rotate Around Y Axis but inverted
+    //        transform.Rotate(transform.up, Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+    //    }
 
-        // Rotate Around X Axis
-        transform.Rotate(Camera.main.transform.right, Vector3.Dot(posDelta, Camera.main.transform.up), Space.World);
-    }
+    //    // Rotate Around X Axis
+    //    transform.Rotate(Camera.main.transform.right, Vector3.Dot(posDelta, Camera.main.transform.up), Space.World);
+    //}
+
+
 }
