8bad5c24e29c7131b43112fa97bcac88a6501e7d
Omar <omar.h.shafeek@gmail.com>
2019-12-26 19:31:38 +0200

    Completed Generalized Controls V1

--- Assets/Omar Assets/Test Box/OrientationNode.cs
+++ Assets/Omar Assets/Test Box/OrientationNode.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class OrientationNode : MonoBehaviour
 {
+    enum OrientationMode
+    {
+        Positional,
+        Rotational
+    }
+
+    enum Axis
+    {
+        X,
+        Y,
+        Z
+    }
+
+    enum ValueEditType
+    {
+        Incrementatl,
+        Decremental
+    }
+
+    OrientationMode orientationMode;
+    
+    private float step = 1;
+
     private void Update()
     {
         if (!SelectionScript.instance.SelectedObject)
@@ -11,47 +34,164 @@ public class OrientationNode : MonoBehaviour
             return;
         }
 
+        adjustSnapStep();
+
+        toggleOrientationMode();
+
+        OrientSelectedObject();
+    }
+
+    private void adjustSnapStep()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadPlus) || Input.GetKeyDown(KeyCode.Plus))
+        {
+            if (orientationMode == OrientationMode.Positional)
+            {
+                step += 0.5f;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.KeypadMinus) || Input.GetKeyDown(KeyCode.Minus))
+        {
+            if (orientationMode == OrientationMode.Positional)
+            {
+                step -= 0.5f;
+            }
+        }
+        step = Mathf.Clamp(step, 0.5f, 1.5f);
+    }
+
+    private void OrientSelectedObject()
+    {
         if (this.gameObject == SelectionScript.instance.SelectedObject)
         {
-            if (Input.GetKeyDown(KeyCode.D))
+            switch (orientationMode)
             {
-                transform.Rotate(transform.up, 90, Space.Self);
-
+                case OrientationMode.Positional:
+                    Move();
+                    break;
+                case OrientationMode.Rotational:
+                    Rotate();
+                    break;
             }
-            else if (Input.GetKeyDown(KeyCode.A))
-            {
-                transform.Rotate(transform.up, -90, Space.Self);
-            }
-            else if (Input.GetKeyDown(KeyCode.W))
-            {
-                transform.Rotate(transform.right, 90, Space.Self);
-            }
-            else if (Input.GetKeyDown(KeyCode.S))
-            {
-                transform.Rotate(transform.right, -90, Space.Self);
-            }
-
-            //if (Input.GetAxis("HorizontalRotate") != 0 || Input.GetAxis("VerticalRotate") != 0)
-            //{
-            //    RotateShape((new Vector3(Input.GetAxis("HorizontalRotate"), Input.GetAxis("VerticalRotate"), 0)) * rotateSpeed);
-            //}
         }
     }
 
-    public void RotateShape(Vector3 posDelta)
+    private void toggleOrientationMode()
     {
-        if (Vector3.Dot(transform.up, Vector3.up) >= 0)
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            // Rotate Around Y Axis
-            transform.Rotate(transform.up, -Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+            switch (orientationMode)
+            {
+                case OrientationMode.Positional:
+                    orientationMode = OrientationMode.Rotational;
+                    break;
+                case OrientationMode.Rotational:
+                    orientationMode = OrientationMode.Positional;
+                    break;
+                default:
+                    break;
+            }
         }
-        else
-        {
-            // Rotate Around Y Axis but inverted
-            transform.Rotate(transform.up, Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
-        }
+    }
 
-        // Rotate Around X Axis
-        transform.Rotate(Camera.main.transform.right, Vector3.Dot(posDelta, Camera.main.transform.up), Space.World);
+    private void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            mapAndPerformOrientation(Axis.X, ValueEditType.Incrementatl);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            mapAndPerformOrientation(Axis.X, ValueEditType.Decremental);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            mapAndPerformOrientation(Axis.Z, ValueEditType.Incrementatl);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            mapAndPerformOrientation(Axis.Z, ValueEditType.Decremental);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            mapAndPerformOrientation(Axis.Y, ValueEditType.Incrementatl);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            mapAndPerformOrientation(Axis.Y, ValueEditType.Decremental);
+        }
+    }
+    private void Rotate()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            mapAndPerformOrientation(Axis.Y, ValueEditType.Decremental);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            mapAndPerformOrientation(Axis.Y, ValueEditType.Incrementatl);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            mapAndPerformOrientation(Axis.X, ValueEditType.Incrementatl);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            mapAndPerformOrientation(Axis.X, ValueEditType.Decremental);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            mapAndPerformOrientation(Axis.Z, ValueEditType.Decremental);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            mapAndPerformOrientation(Axis.Z, ValueEditType.Incrementatl);
+        }
+    }
+
+    private void mapAndPerformOrientation(Axis axis, ValueEditType valueEditType)
+    {
+        switch (axis)
+        {
+            case Axis.X:
+                orientSelectedObject(true, false, false, valueEditType == ValueEditType.Incrementatl);
+                break;
+            case Axis.Y:
+                orientSelectedObject(false, true, false, valueEditType == ValueEditType.Incrementatl);
+                break;
+            case Axis.Z:
+                orientSelectedObject(false, false, true, valueEditType == ValueEditType.Incrementatl);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void orientSelectedObject(bool x, bool y, bool z, bool sign)
+    {
+
+        if (this.gameObject == SelectionScript.instance.SelectedObject)
+        {
+            switch (orientationMode)
+            {
+                case OrientationMode.Positional:
+
+                    transform.position = new Vector3(
+                        (x == true ? transform.position.x + (sign == true ? step : -step) : transform.position.x),
+                        (y == true ? transform.position.y + (sign == true ? step : -step) : transform.position.y),
+                        (z == true ? transform.position.z + (sign == true ? step : -step) : transform.position.z)
+                        );
+                    break;
+                case OrientationMode.Rotational:
+
+                    transform.Rotate(
+                        (x == true ? Vector3.right :
+                        (y == true ? Vector3.up :
+                        (z == true ? Vector3.forward : Vector3.zero))),
+                        (sign == true ? 90 : -90),
+                        Space.World);
+                    break;
+            }
+        }
     }
 }
