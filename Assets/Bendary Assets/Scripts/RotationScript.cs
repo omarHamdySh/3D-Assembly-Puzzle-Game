04e436bfd386f1e42277cf735a4a602a0fea1a43
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-12-26 11:54:03 +0200

    bendary
    finish object Rotation Mechanic

--- Assets/Bendary Assets/Scripts/RotationScript.cs
+++ Assets/Bendary Assets/Scripts/RotationScript.cs
@@ -1,39 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
-public class RotationScript : Editor
+public class RotationScript : MonoBehaviour
 {
-    //private Vector3 posDelta = Vector3.zero;
+    [SerializeField] private float rotateSpeed = 0;
 
-    //private void OnMouseDown()
-    //{
-    //    posDelta = Input.mousePosition;
-    //}
+    [HideInInspector] public bool IsSelected = false;
 
-    //private void OnMouseDrag()
-    //{
-    //    posDelta = Input.mousePosition - posDelta;
-    //    RotateShape(posDelta);
-    //}
+    private void Update()
+    {
+        if (IsSelected)
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                RotateShape((new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0)) * rotateSpeed);
+            }
+        }
+    }
 
-    //public void RotateShape(Vector3 posDelta)
-    //{
-    //    if (Vector3.Dot(transform.up, Vector3.up) >= 0)
-    //    {
-    //        // Rotate Around Y Axis
-    //        transform.Rotate(transform.up, -Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
-    //    }
-    //    else
-    //    {
-    //        // Rotate Around Y Axis but inverted
-    //        transform.Rotate(transform.up, Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
-    //    }
+    public void RotateShape(Vector3 posDelta)
+    {
+        if (Vector3.Dot(transform.up, Vector3.up) >= 0)
+        {
+            // Rotate Around Y Axis
+            transform.Rotate(transform.up, -Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+        }
+        else
+        {
+            // Rotate Around Y Axis but inverted
+            transform.Rotate(transform.up, Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+        }
 
-    //    // Rotate Around X Axis
-    //    transform.Rotate(Camera.main.transform.right, Vector3.Dot(posDelta, Camera.main.transform.up), Space.World);
-    //}
+        // Rotate Around X Axis
+        transform.Rotate(Camera.main.transform.right, Vector3.Dot(posDelta, Camera.main.transform.up), Space.World);
+    }
 
 
+    private void OnMouseDown()
+    {
+        if (SelectionScript.instance.SelectedObject)
+        {
+            SelectionScript.instance.SelectedObject.GetComponent<RotationScript>().IsSelected = false;
+            SelectionScript.instance.SelectedObject.GetComponent<RotationScript>().DisableSelectionOutline();
+        }
+
+        SelectionScript.instance.SelectedObject = gameObject;
+
+        IsSelected = true;
+        ShowSelectionOutline();
+    }
+
+    public void ShowSelectionOutline()
+    {
+
+    }
+
+    public void DisableSelectionOutline()
+    {
+
+    }
 }
---
+++ Assets/Bendary Assets/Scripts/SelectionScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionScript : MonoBehaviour
+{
+    #region Singleton
+    public static SelectionScript instance { private set; get; }
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    [HideInInspector] public GameObject SelectedObject;
+}
