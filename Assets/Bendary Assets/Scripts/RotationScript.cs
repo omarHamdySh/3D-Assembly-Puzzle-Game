38d5aea89722e187793f8505000dbad63b105dac
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-12-26 08:58:43 +0200

    bendary
    make my rotation algorithm

--- Assets/Bendary Assets/Scripts/RotationScript.cs
+++ Assets/Bendary Assets/Scripts/RotationScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 posDelta = Vector3.zero;
+
+    private void OnMouseDown()
     {
-        
+        posDelta = Input.mousePosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
     {
-        
+        posDelta = Input.mousePosition - posDelta;
+        RotateShape(posDelta);
+    }
+
+    public void RotateShape(Vector3 posDelta)
+    {
+        if (Vector3.Dot(transform.up, Vector3.up) >= 0)
+        {
+            // Rotate Around Y Axis
+            transform.Rotate(transform.up, -Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+        }
+        else
+        {
+            // Rotate Around Y Axis but inverted
+            transform.Rotate(transform.up, Vector3.Dot(posDelta, Camera.main.transform.right), Space.World);
+        }
+
+        // Rotate Around X Axis
+        transform.Rotate(Camera.main.transform.right, Vector3.Dot(posDelta, Camera.main.transform.up), Space.World);
     }
 }
