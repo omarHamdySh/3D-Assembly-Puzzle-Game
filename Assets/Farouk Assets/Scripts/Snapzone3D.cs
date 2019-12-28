70474a89d8ab168b509310186b37c7fb19b9fcda
Ahmed <Ahmos42@gmail.com>
2019-12-28 10:52:31 +0200

    Farouk
    Snapzone2

--- Assets/Farouk Assets/Scripts/Snapzone3D.cs
+++ Assets/Farouk Assets/Scripts/Snapzone3D.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SnapZone3D : MonoBehaviour
 {
+    [SerializeField]
+    private string dragObjectTag ;
     private void OnTriggerStay(Collider collision)
     {
 
-        if (!collision.GetComponent<moveByDrag>().dragged && collision.gameObject.tag == "Player")
+        if (!collision.GetComponent<moveByDrag>().dragged && collision.gameObject.tag == dragObjectTag)
         {
             collision.transform.position = transform.position;
         }
