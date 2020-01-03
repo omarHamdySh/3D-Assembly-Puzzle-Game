c205561ce86d1282a212fa42bf14adf23d0a0d2a
OmarHamdySh <omar.h.shafeek@gmail.com>
2020-01-03 16:58:41 +0200

    Omar - After Adding Game Logic Script
    Game Logic is counting good.
    If playr works fire works will fire we need to make the camera stick to the cow front view on order to make the player sees the fireworks.
    
    We can now use the time manager to preview the game time and make logic of winning or loosing.

---
+++ Assets/Omar Assets/Test Box/GameLogicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameLogicManager : MonoBehaviour
+{
+    public UnityEvent playerWins, PlayerLoses;
+    public GameObject fireWorks;
+    public List<SnapZone3D_Omar> activeSnapZones;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManager.Instance)
+        {
+            SnapZone3D_Omar[] snapZones = GameObject.FindObjectsOfType<SnapZone3D_Omar>();
+            foreach (var snapZone in snapZones)
+            {
+                activeSnapZones.Add(snapZone);
+                snapZone.OnSnappedEvent += removeOnSnap;
+            }
+        }
+    }
+
+    public void removeOnSnap(SnapZone3D_Omar snapZone) {
+        //Remove the snapZone from active snap zones list which will remain the playing logic.
+        activeSnapZones.Remove(snapZone);
+        
+        if (activeSnapZones.Count==0)
+        {
+            playerWins.Invoke();
+        }
+    }
+}
---
+++ Assets/Omar Assets/Test Box/TimeUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class TimeUI : MonoBehaviour
+{
+    TextMeshProUGUI timeText;
+    private void Start()
+    {
+        timeText = GetComponent<TextMeshProUGUI>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timeText.text = "Time in Seconds:" + GameManager.Instance.timeManager.gameTime.realSecond;
+    }
+}
