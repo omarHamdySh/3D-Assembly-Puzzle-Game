3c13c368a2940784f684f887eb2da5ff440c71ae
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2020-01-03 18:28:49 +0200

    bendary - Intgrate with Omar Scenes

--- Assets/Omar Assets/Test Box/TimeUI.cs
+++ Assets/Omar Assets/Test Box/TimeUI.cs
@@ -4,15 +4,15 @@ using TMPro;
 using UnityEngine;
 public class TimeUI : MonoBehaviour
 {
-    TextMeshProUGUI timeText;
-    private void Start()
-    {
-        timeText = GetComponent<TextMeshProUGUI>();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        if(GameManager.Instance)
-            timeText.text = "Time(sec):" + (Mathf.Floor(GameManager.Instance.timeManager.gameTime.realSecond)+1);
-    }
+    //TextMeshProUGUI timeText;
+    //private void Start()
+    //{
+    //    timeText = GetComponent<TextMeshProUGUI>();
+    //}
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if(GameManager.Instance)
+    //        timeText.text = "Time(sec):" + (Mathf.Floor(GameManager.Instance.timeManager.gameTime.realSecond)+1);
+    //}
 }
