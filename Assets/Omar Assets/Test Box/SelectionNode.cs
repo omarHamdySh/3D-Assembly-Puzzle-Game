adfc7cbfeca0fcae20374962fefdd0cbdf76b658
Omar <omar.h.shafeek@gmail.com>
2020-01-15 23:31:33 +0200

    Omar - Neglect Rotation Component

--- Assets/Omar Assets/Test Box/SelectionNode.cs
+++ Assets/Omar Assets/Test Box/SelectionNode.cs
@@ -4,34 +4,8 @@ using UnityEngine;
 
 public class SelectionNode : MonoBehaviour
 {
-
-    private void OnMouseDown()
-    {
-        //if (SelectionScript.instance.SelectedObject)
-        //{
-        //    SelectionScript.instance.SelectedObject.GetComponent<SelectionNode>().DisableSelectionOutline();
-        //}
-
-        //SelectionScript.instance.SelectedObject = gameObject;
-
-        //ShowSelectionOutline();
-    }
-    public void ShowSelectionOutline()
-    {
-
-    }
-
-    public void DisableSelectionOutline()
-    {
-
-    }
-
     public void select() {
         SelectionManager.instance.selectedObject = gameObject;
-    }
-
-    public void unSelect() { 
-    
     }
 
 }
