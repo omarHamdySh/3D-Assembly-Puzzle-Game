e42817292b4eb2fbc1040def6a6786b2daba3074
Ahmed <Ahmos42@gmail.com>
2020-01-18 01:45:05 +0200

    Farouk-Finishing the game -Added Sound Control ON/OFF +How2Play and Controls Panels + Completed the UI

---
+++ Assets/Farouk Assets/Scripts/SoundControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControl : MonoBehaviour
+{
+    public void SoundON()
+    {
+        AudioListener.pause = false;
+    }
+    public void SoundOFF()
+    {
+        AudioListener.pause = true;
+    }
+}
